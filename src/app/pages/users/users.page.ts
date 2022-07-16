59c12a30756bcb4af18605ad4e29066610629137
Nothing <zackagarwal@gmail.com>
2022-07-16 17:56:44 +0530

    (add): users

--- src/app/pages/users/users.page.ts
+++ src/app/pages/users/users.page.ts
@@ -19,10 +19,7 @@ export class UsersPage implements OnInit {
   dom: 'frtlp',
     responsive:true,
     ordering:false,
-    columnDefs:[
-      { orderable: false, targets: 0 }
-    ],
-    order: [[1, 'desc']],
+
 
     language: {
       search: "Search :",
